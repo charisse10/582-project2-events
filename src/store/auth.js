e6157d2355047349ee02e55ba0e70a719fdf4c95
charisse10 <1995510@edu.vaniercollege.qc.ca>
2023-08-24 17:01:15 -0400

    initial file setup

---
+++ src/store/auth.js
@@ -0,0 +1,12 @@
+import { defineStore } from "pinia";
+
+export const useAuthStore = defineStore("auth", {
+  state: () => ({
+    userType: "null",
+  }),
+  actions: {
+    setUserType(userType) {
+      this.userType = userType;
+    },
+  },
+});
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,31 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button @click="setUserType('user')">User</button>
+    <button @click="setUserType('admin')">Admin</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import { useAuthStore } from "@/store/auth";
 
 export default {
   name: "HomeView",
   components: {
     HelloWorld,
+  },
+  setup() {
+    const authStore = useAuthStore();
+
+    const setUserType = (userType) => {
+      authStore.setUserType(userType);
+    };
+
+    return {
+      setUserType,
+    };
   },
 };
 </script>

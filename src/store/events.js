2d3042be2970d50bd4b11a94b558e119a3fd8c03
charisse10 <1995510@edu.vaniercollege.qc.ca>
2023-09-08 18:33:16 -0400

    modifying events store for interested buttons

--- src/store/events.js
+++ src/store/events.js
@@ -1,8 +1,37 @@
+// import { defineStore } from "pinia";
+
+// export const useEventsStore = defineStore("events", {
+//   state: () => ({
+//     workingURL:
+//       "https://probable-guacamole-w6r64q77rpqcg9rv-3000.app.github.dev/",
+//   }),
+// });
+
+
 import { defineStore } from "pinia";
 
-export const useEventsStore = defineStore("events", {
+export const useInterestedEventsStore = defineStore("interestedEventsStore", {
   state: () => ({
-    workingURL:
-      "https://probable-guacamole-w6r64q77rpqcg9rv-3000.app.github.dev/",
+    interestedEvents: [],
+    interestedEventCount: 0,
   }),
+
+  actions: {
+    addInterestedEvent(event) {
+      if (!this.interestedEvents.some((e) => e._id === event._id)) {
+        this.interestedEvents.push({ ...event, isInterested: true });
+        this.interestedEventCount++;
+      }
+    },
+
+    deleteInterestedEvent(event) {
+      const index = this.interestedEvents.findIndex(
+        (interestedEvent) => interestedEvent._id === event._id
+      );
+      if (index !== -1) {
+        this.interestedEvents.splice(index, 1);
+        this.interestedEventCount--;
+      }
+    },
+  },
 });
